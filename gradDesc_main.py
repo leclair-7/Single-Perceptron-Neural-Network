f5ff7440bdb8ee99d4d1c7a661f64aabe8fdb3d9
Wolfgang <wolf_leclair7@tutanota.com>
2016-03-06 09:17:40 -0600

    Allow user to specify number of iterations

--- gradDesc_main.py
+++ gradDesc_main.py
@@ -41,9 +41,11 @@ data = np.resize( data, ( row ,column ))
 def sigmoid(z):
         #applies the sigmoid activation function
         return 1.0/(1.0 + np.exp(-z))
+
 def sigmoindPrime(z):
         #derivative of sigmoid function        
         return sigmoid(z) * (1.0- sigmoid(z))
+
 def forward(w1, row, data):        
         #input layer matrix + hidden layer matrix                
         z2 = np.dot( data[row][:-1], w1)
@@ -66,13 +68,11 @@ numIterations = 3
 for iteration in range(numIterations):
     for k in range(len(data)):
         for i in range(len(w1)):            
-            yHypoth = sigmoid(  (np.dot( data[k][:-1], w1) ) )
-           
+            yHypoth = sigmoid(  (np.dot( data[k][:-1], w1) ) )           
             yActual = data[k][-1]
             data_k_i =  ( data[k][i] ) 
             sigma_wx = sigmoid(np.dot( data[k][:-1], w1) ) 
-            #w1[i] = w1[i] + alpha * ( yActual - yHypoth) *(data_k_i)*(sigma_wx)*(sigma_wx - 1) #+ (data[k][i])**2  added to the last bit in
-            w1[i] = w1[i] + alpha * ( yActual - yHypoth) *(data_k_i)*sigmoindPrime( np.dot(data[k][:-1], w1))
+            w1[i] = w1[i] + alpha * ( yActual - yHypoth) * (data_k_i) * sigmoindPrime( np.dot(data[k][:-1], w1))
     printCount = 0
     for t in range(len(titles)):
         if t == 0:
@@ -81,10 +81,7 @@ for iteration in range(numIterations):
             print(", output= %.3f" % np.dot( data[k][:-1], w1),sep='' )
         else:
             print(", w(", titles[t],")= %.3f" % w1[t],end="",sep='' )
-        
-
-#(np.dot( data[k][:-1], w1)     
-    
+   
 numRight =0
 numWrong = 0
 for j in range(len(data)):
