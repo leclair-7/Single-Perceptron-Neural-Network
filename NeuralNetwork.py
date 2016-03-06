261f7dddae8ba533f6669f79d36764eefe2f949d
Wolfgang <wolf_leclair7@tutanota.com>
2016-03-06 11:37:14 -0600

    Test set function actually works

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,30 +1,44 @@
 # - Created by Lucas Hagel - 
-#this nonworking iteration of the Neuralnetwork class
-#is a first take of a few to get this project working
+#this iteration of the Neuralnetwork class
+#is a single perceptron gradient descent NN
 #
 
 import numpy as np
 
-
 class NeuralNetwork(object):
-    def __init__(self,trainFileName, alphaUpdate,iteration):
+    def __init__(self,trainFileName, testFile, alphaUpdate,iteration):
         self.alpha = alphaUpdate
-        self.data = np.array([])
         
         #row and column are rewritten in getDataFromFile
         #these two are just here for awaremess
-        self.row = 0
-        self.column = 3
+        #self.row = 0
+        #self.column = 3
         self.NumIterations = iteration
-        self.getDataFromFile(trainFileName)
+        
+        
+        self.data = np.loadtxt(trainFileName,skiprows=1)
 
+        fh = open(trainFileName,'r')
+        for i,line in enumerate(fh):
+            if i is 1: break
+            self.titles = line.split()
+        
+        
+        '''
+        self.data = self.getDataFromFile(trainFileName)
+        
+        '''        
+        self.testSet = np.loadtxt(testFile,skiprows=1)
+        
+        
         #defines hyperparameters
-        self.inputLayerSize = self.column - 1
+        self.inputLayerSize = len(self.data[0]) - 1
         self.outputLayerSize = 1
         self.hiddenLayerSize = 1
         
         #NN's learn the larameters
         self.w1 =np.zeros( [self.inputLayerSize ,1])
+        #print(self.w1)        
         #self.w2 =np.array( self.hiddenLayerSize ,1)
         self.w2 =np.array( [1])
         
@@ -45,6 +59,7 @@ class NeuralNetwork(object):
             if line:
                 yield line
     def getDataFromFile(self,filename):
+        data = np.array([])
         count = 0        
         with open(filename,'r') as ofo:    
             for line in self.nonblank_lines(ofo):        
@@ -55,14 +70,16 @@ class NeuralNetwork(object):
                 else:
                     count += 1
                     currLine = line.split()            
-                    row = len(self.data)
-                    self.data = np.append( self.data, currLine)
-                    self.data = np.resize( self.data, (row + 1, column))
+                    row = len(data)
+                    data = np.append( data, currLine)
+                    data = np.resize( data, (row + 1, column))
                     currLine = []
-        self.row = len(self.data)
-        self.column = len(self.data[0])
-        self.data = [ float(i) for line in self.data for i in line ]
-        self.data = np.resize( self.data, ( self.row ,self.column ))  
+        #self.row = len(data)
+        #self.column = len(self.data[0])
+        data = [ float(i) for line in data for i in line ]
+        data = np.resize( data, ( row ,column ))
+        print(data)        
+        return data
     '''
     End of File/dataset entry functions
     '''
@@ -75,40 +92,58 @@ class NeuralNetwork(object):
         return self.sigmoid(z) * (1.0 - self.sigmoid(z))
 
         
-    def forward(self, w1, row, data):        
+    def forward(self, weightVector, row, dataset):        
         #input layer matrix + hidden layer matrix                
-        z2 = self.sigmoid( np.dot( self.data[row][:-1], w1) )        
+        z2 = self.sigmoid( np.dot( dataset[row][:-1], weightVector) )        
         if z2 > .5:
             return 1.0
         else:
             return 0.0
     def trainNN(self):
-        for iteration in range(self.NumIterations):
-            for k in range(len(self.data)):
-                for i in range(len(self.w1)):            
-                    yHypoth = self.sigmoid(  (np.dot( self.data[k][:-1], self.w1) ) )
-           
-                    yActual = self.data[k][-1]
-                    data_k_i =  ( self.data[k][i] ) 
-                    #sigma_wx = self.sigmoid(np.dot( self.data[k][:-1], self.w1) ) 
-                    #w1[i] = w1[i] + alpha * ( yActual - yHypoth) *(data_k_i)*(sigma_wx)*(sigma_wx - 1) #+ (data[k][i])**2  added to the last bit in
-                    self.w1[i] = self.w1[i] + self.alpha * ( yActual - yHypoth) *(data_k_i)*self.sigmoindPrime( np.dot(self.data[k][:-1], self.w1))
+        #for iteration in range(self.NumIterations):
+        #len(self.data)
+        for k in range(self.NumIterations):
+            newWeight = self.w1.copy()                
+            for i in range(len(self.w1)):            
+                yHypoth = self.sigmoid(  (np.dot( self.data[k % len(self.data)][:-1], self.w1) ) )           
+                yActual = self.data[k % len(self.data)][-1]
+                data_k_i =  ( self.data[k % len(self.data)][i] ) 
+                #sigma_wx = self.sigmoid(np.dot( self.data[k][:-1], self.w1) ) 
+                #w1[i] = w1[i] + alpha * ( yActual - yHypoth) *(data_k_i)*(sigma_wx)*(sigma_wx - 1) #+ (data[k][i])**2  added to the last bit in
+                newWeight[i] = self.w1[i] + self.alpha * (yActual - yHypoth) *(data_k_i)*self.sigmoindPrime( np.dot(self.data[k% len(self.data)][:-1], self.w1))
+                #print(self.w1)
+            self.w1 = newWeight.copy()
+            #print( self.w1)
             for t in range(len(self.titles)):
                 if t == 0:
-                    print("After iteration",t+1,": w(", self.titles[t],")= %.3f" % self.w1[t],end="",sep = '')
+                    print("After iteration",k+1,": w(", self.titles[t],")= %.3f" % self.w1[t],end="",sep = '')
                 elif t == len(self.titles)-1:
-                    print(", output= %.3f" % np.dot( self.data[k][:-1], self.w1),sep='' )
+                    print(", output= %.3f" % self.sigmoid(np.dot( self.data[k % len(self.data)][:-1], self.w1) ),sep='' )
                 else:
                     print(", w(", self.titles[t],")= %.3f" % self.w1[t],end="",sep='' )
         numRight =0.0
         numWrong = 0.0
         for j in range(len(self.data)):
             yHypoth = self.forward(self.w1,j,self.data)
-            if self.data[j][-1] == yHypoth:
+            #if self.data[j][-1] == yHypoth:
+            if int(self.data[j][-1]) == int(yHypoth):
                 numRight +=1.0
             else:
                 numWrong += 1.0
-        print("\nAccuracy on training set %.1f" % ((numRight/(numRight + numWrong)) * 100.0),"%",sep='' )
+        print("\nAccuracy on training set (",self.NumIterations," instances): ","%.1f" % ((numRight/(numRight + numWrong)) * 100.0),"%",sep='' )
+    #it would be cool if this were written to take a numpy input as an argument
+    def test(self):
+        numRight =0.0
+        numWrong = 0.0
+        for j in range(len(self.testSet)):
+            yHypoth = self.forward(self.w1,j,self.testSet)
+            #print("Hypothe", int(yHypoth), " actual is: ", int(self.testSet[j][-1])  )
+            if ((int(self.testSet[j][-1]) ) == (int(yHypoth)) ):
+                numRight +=1.0
+            else:
+                numWrong += 1.0
+        print("\nAccuracy on test set (",len(self.testSet)," instances): ", "%.1f" % ((numRight/(numRight + numWrong)) * 100.0),"%",sep='' )
+        
 
 
     
--- gradDesc_main.py
+++ gradDesc_main.py
@@ -9,92 +9,19 @@ Created on Wed Mar  2 20:26:05 2016
 import numpy as np
 from NeuralNetwork import *
 
-titles=[]
-data = np.array([])
-
-count =0
-
-def nonblank_lines(f):
-    for l in f:
-        line = l.rstrip()
-        if line:
-            yield line
-            
-with open("train2-win.dat",'r') as ofo:    
-    for line in nonblank_lines(ofo):        
-        if len(line.split()) != 0 and count ==0:
-            count+=1
-            titles = line.split()
-            column = len(titles)            
-        else:
-            count += 1
-            currLine = line.split()            
-            row = len(data)
-            data = np.append( data, currLine)
-            data = np.resize( data, (row + 1, column))
-            currLine = []
-row = len(data)
-column = len(data[0])
-data = [ float(i) for line in data for i in line ]
-data = np.resize( data, ( row ,column ))            
-            
-def sigmoid(z):
-        #applies the sigmoid activation function
-        return 1.0/(1.0 + np.exp(-z))
-
-def sigmoindPrime(z):
-        #derivative of sigmoid function        
-        return sigmoid(z) * (1.0- sigmoid(z))
-
-def forward(w1, row, data):        
-        #input layer matrix + hidden layer matrix                
-        z2 = np.dot( data[row][:-1], w1)
-        a2 =sigmoid(z2)
-        if a2 > .5:
-            return 1.0
-        else:
-            return 0.0
-
 '''
-for i in range(1000):
-    alphaBest = .00001
+data = np.loadtxt("train2-win.dat",skiprows=1)
+
+fh = open("train2-win.dat",'r')
+for i,line in enumerate(fh):
+    if i is 1: break
+    titles = line.split()
+fh.close()
 '''
 
-w1 = np.zeros( len( data[0]) -1 )
-w1 = [ float(i) for i in w1]
-alpha = .9
-
-numIterations = 3
-for iteration in range(numIterations):
-    for k in range(len(data)):
-        for i in range(len(w1)):            
-            yHypoth = sigmoid(  (np.dot( data[k][:-1], w1) ) )           
-            yActual = data[k][-1]
-            data_k_i =  ( data[k][i] ) 
-            sigma_wx = sigmoid(np.dot( data[k][:-1], w1) ) 
-            w1[i] = w1[i] + alpha * ( yActual - yHypoth) * (data_k_i) * sigmoindPrime( np.dot(data[k][:-1], w1))
-    printCount = 0
-    for t in range(len(titles)):
-        if t == 0:
-            print("After iteration",t+1,": w(", titles[t],")= %.3f" % w1[t],end="",sep = '')
-        elif t == len(titles)-1:
-            print(", output= %.3f" % np.dot( data[k][:-1], w1),sep='' )
-        else:
-            print(", w(", titles[t],")= %.3f" % w1[t],end="",sep='' )
-   
-numRight =0
-numWrong = 0
-for j in range(len(data)):
-    yHypoth = forward(w1,j,data)
-    if data[j][-1] == yHypoth:
-        numRight +=1
-    else:
-        numWrong += 1
-print("\nAccuracy on training set", numRight/(numRight + numWrong)," for accuracy" )
-
-NN_uno = NeuralNetwork("train2-win.dat",.3, 3)
+NN_uno = NeuralNetwork("train5-win.dat","test5-win.dat",.9, 60)
 NN_uno.trainNN()
-    
-    
+NN_uno.test()
+
     
             
